6f619f913ceca97f8e7aaf1cb8060cb1b0cf1398
l_ziege <lucasziegemann@gmail.com>
2024-06-26 20:51:01 -0300

    Nueva prueba del deploy

--- manage.py
+++ manage.py
@@ -11,34 +11,10 @@ def main():
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "   
+            "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    
-    from django.core.wsgi import get_wsgi_application
-    application = get_wsgi_application()
-    from gunicorn.app.base import BaseApplication
-
-    class StandaloneApplication(BaseApplication):
-        def __init__(self, app, options=None):
-            self.options = options or {}
-            self.application = app
-            super().__init__()
-
-        def load_config(self):
-            for key, value in self.options.items():
-                if key in self.cfg.settings and value is not None:
-                    self.cfg.set(key, value)
-
-        def load(self):
-            return self.application
-
-    if __name__ == '__main__':
-        options = {
-            'bind': '0.0.0.0:8000',
-            'workers': 3
-        }
-        StandaloneApplication(application, options).run()
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
