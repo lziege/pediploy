1d226a426a616cb5af043bd5f55471c23ee4cb30
l_ziege <lucasziegemann@gmail.com>
2024-06-27 03:09:01 -0300

    Algunas modificaciones para ejecutar tanto el bot como el servidor de django en vercel

---
+++ index.py
@@ -0,0 +1,32 @@
+import os
+import django
+import asyncio
+from threading import Thread
+from flask import Flask
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+django.setup()
+
+from src.backend.management.commands.run_telegram_bot import start_bot
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Bot is running'
+
+def run_flask():
+    app.run(host='0.0.0.0', port=5000)
+
+def run_bot():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(start_bot())
+
+if __name__ == '__main__':
+    bot_thread = Thread(target=run_bot)
+    bot_thread.start()
+    flask_thread = Thread(target=run_flask)
+    flask_thread.start()
+    bot_thread.join()
+    flask_thread.join()
