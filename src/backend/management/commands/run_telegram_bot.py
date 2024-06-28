d18ddaecded961bef8e291e5979f251d00a315c7
l_ziege <lucasziegemann@gmail.com>
2024-06-28 01:18:57 -0300

    Pruebaaaa

--- src/backend/management/commands/run_telegram_bot.py
+++ src/backend/management/commands/run_telegram_bot.py
@@ -662,4 +662,6 @@ def start_bot():
     applicationBuilder.add_handler(individual_order_handler)
     applicationBuilder.add_handler(orders_record_handler)
 
+    applicationBuilder.run_polling()
+
     return applicationBuilder
